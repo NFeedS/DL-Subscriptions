a69dcddfb256502847edc0a49e2c216c20df031e
Leonardo Malcotti <leonardo.malcotti@studenti.unitn.it>
2023-05-21 15:16:52 +0200

    added crud operations

---
+++ src/main/java/com/nfeeds/data/subscriptions/controller/SubscriptionsController.java
@@ -0,0 +1,51 @@
+package com.nfeeds.data.subscriptions.controller;
+
+import com.nfeeds.data.subscriptions.model.Subscription;
+import com.nfeeds.data.subscriptions.repository.SubscriptionRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+
+@Log4j2
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/v1/subscriptions")
+public class SubscriptionsController {
+	
+	private final SubscriptionRepository subscriptionRepository;
+	
+	@PostMapping("/")
+	public void newSubscription(@RequestBody Subscription body){
+		log.debug(this.getClass().getSimpleName() + " - newSubscription");
+		subscriptionRepository.save(body);
+	}
+	
+	@GetMapping("/{id}")
+	public Subscription getSubscription(@PathVariable("id") String sub_id){
+		log.debug(this.getClass().getSimpleName() + " - getSubscription");
+		return subscriptionRepository.findById(sub_id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Subscription " + sub_id + " not found"));
+	}
+	
+	@GetMapping("user/{id}")
+	public List<Subscription> getSubscriptionByUser(@PathVariable("id") String user_id){
+		log.debug(this.getClass().getSimpleName() + " - getSubscriptionByUser");
+		return subscriptionRepository.getSubscriptionsByUserId(user_id);
+	}
+	
+	@GetMapping("/topic/{id}")
+	public List<Subscription> getSubscriptionByTopic(@PathVariable("id") String topic_id){
+		log.debug(this.getClass().getSimpleName() + " - getSubscriptionByTopic");
+		return subscriptionRepository.getSubscriptionsByTopicId(topic_id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteSubscription(@PathVariable("id") String sub_id){
+		log.debug(this.getClass().getSimpleName() + " - deleteSubscription");
+		subscriptionRepository.deleteById(sub_id);
+	}
+}
--- src/main/java/com/nfeeds/data/subscriptions/repository/SubscriptionRepository.java
+++ src/main/java/com/nfeeds/data/subscriptions/repository/SubscriptionRepository.java
@@ -1,45 +1,16 @@
 package com.nfeeds.data.subscriptions.repository;
 
 import com.nfeeds.data.subscriptions.model.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
-/**
- * <p> Interface used by Spring Data Rest to create CRUD endpoints over the User table. </p>
- * <p> The base endpoints generated will have the form: </p>
- * <code>
- * <p><i> GET|POST : {basePath}/subscriptions </i></p>
- * <p><i> GET|POST|PUT|DELETE : {basePath}/subscriptions/{id} </i></p>
- * </code>
- * <p>(plus the /profile and OPTIONS endpoint to get the endpoints metadata and options) </p>
- * <p>
- * <p> Additional endpoint will be created based on the query functions of the interface: </p>
- * <code><p><i> GET : {basePath}/subscriptions/search/{specified_path|function_name}?{parameters} </i></p></code>
- * <p>
- * <p> The response of the endpoint will follow HATEOAS specifications. </p>
- */
-public interface SubscriptionRepository extends CrudRepository<Subscription, String> {
-
-    /**
-     * <p> Search all the subscriptions of a certain user.</p>
-     * <code><p><i> GET : {basePath}/subscriptions/search/user?id={user_id} </i></p></code>
-     * <p>
-     * @param user_id The id of the user of the subscriptions searched.
-     * @return The list of subscriptions that belong to the user indicated by user_id.
-     */
-    @RestResource(path = "user" ,rel = "byUser")
-    List<Subscription> findByUserId(@Param("id") String user_id);
-
-    /**
-     * <p> Search all the subscriptions to a certain topic.</p>
-     * <code><p><i> GET : {basePath}/subscriptions/search/topic?id={topic_id} </i></p></code>
-     * <p>
-     * @param topic_id The id of the topic of the subscriptions searched.
-     * @return The list of subscriptions that have to the topic indicated by topic_id.
-     */
-    @RestResource(path = "topic", rel = "byTopic")
-    List<Subscription> findByTopicId(@Param("id") String topic_id);
+public interface SubscriptionRepository extends JpaRepository<Subscription, String> {
+    
+    List<Subscription> getSubscriptionsByUserId(String user_id);
+    
+    List<Subscription> getSubscriptionsByTopicId(String topic_id);
 }
